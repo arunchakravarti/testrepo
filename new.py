409f3c44d04fbefd24df692fbb913580b4eaf9f2
arun kumar chakravarti <89254702+arunchakravarti@users.noreply.github.com>
2022-08-14 13:09:29 +0600

    Add files via upload

---
+++ new.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+'My Jupyter Notebook on IBM Watson Studio'
+
+
+# In[3]:
+
+
+'Arun Kumar Chakravarti'
+
+
+# In[4]:
+
+
+'Data Scientist'
+
+
+# In[5]:
+
+
+'I am interested in Data Science because I would love to contribute towards affordable and quality healthcare in the future'
+
+
+# In[7]:
+
+
+'The below should print Hello World'
+
+
+# In[8]:
+
+
+print("Hello World!")
+
+
+# In[ ]:
+
+
+
+

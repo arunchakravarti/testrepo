2426109e51bfd12397cdd136a36af882575974c2
arun kumar chakravarti <89254702+arunchakravarti@users.noreply.github.com>
2022-08-14 12:25:52 +0600

    Add files via upload

---
+++ firstpython.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("My Jupyter Notebook on IBM Watson Studio")
+
+
+# In[2]:
+
+
+print("I am Arun Kumar Chakravarti")
+
+
+# In[3]:
+
+
+print("I am interested in data science because I really want to learn and to progress in my career. Programming requires constant learning and improving. Taking this course will help me to learn and study this Data Science and also to implement it. It can help me advance in my knowledge. This course will help me in defining Data Science, understanding how Python could potentially impact our business and industry, to write a thought leadership piece regarding use cases and industry potential of Data Science, explain Data Science to clients, friends, joining a community of economists, business leaders, entrepreneurs, and technologists that are shaping this technology as we speak.")
+
+
+# In[4]:
+
+
+def guess(n):
+    return (n*(n+1))/2
+guess(100)==sum(range(101))
+
+
+# In[5]:
+
+
+print("~This is a final peer assignment for this course~")
+
+
+# In[ ]:
+
+
+
+
